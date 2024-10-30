4254afe0866024d19024984a56cb1b0d1b36bbb1
YoelAharoni12 <yoel345@gmail.com>
2024-10-30 13:44:42 +0200

    add watcher to collection db and add WS for real time updated

--- src/http/controllers/promotion.controler.ts
+++ src/http/controllers/promotion.controler.ts
@@ -5,10 +5,7 @@ import {PromotionService} from "../services/promotion.service";
 export const getPromotions = async (req: Request, res: Response) => {
     try {
         const {page = 1, limit = 50} = req.query;
-        console.log(req.query)
         const promotions = await PromotionService.getPromotions(+page, +limit);
-        const all = await PromotionService.getAllPromotions()
-        console.log(all);
         res.json(promotions);
     } catch (error) {
         res.status(500).json({message: 'Error fetching promotions'});
--- src/http/services/promotion.service.ts
+++ src/http/services/promotion.service.ts
@@ -1,10 +1,8 @@
 import {promotionModel} from "../../db/model/promotions.model";
+import {Promotion} from "../../models/promotion.model";
 
 export const PromotionService = {
-    getPromotions: async (page: number, limit: number) => {
+    getPromotions: async (page: number, limit: number): Promise<Promotion[]> => {
         return promotionModel.find().skip((page - 1) * limit).limit(limit);
-    },
-    getAllPromotions: async () => {
-        return promotionModel.find({}).lean().exec();
     }
 };
